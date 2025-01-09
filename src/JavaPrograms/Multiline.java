5d0f89f6e446cdb9ae7f2a2d3fea6819e9747fbe
Venkata Sriram, Gundu <Gundu.VenkataSriram@cba.com.au>
2025-01-09 21:47:46 +1100

    Added few more programs

---
+++ src/JavaPrograms/Multiline.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+
+public class Multiline
+{
+    public static void main(String[] args) {
+
+        /*
+        *This is multiline comment
+        *
+        *
+        *
+         */
+
+        //This is single line comment
+        System.out.println("Multiline comment");
+    }
+}
---
+++ src/JavaPrograms/StringProgram.java
@@ -0,0 +1,9 @@
+package JavaPrograms;
+
+public class StringProgram
+{
+    public static void main(String[] args) {
+        String s="gv";
+        System.out.println(s+"sriram");
+    }
+}
