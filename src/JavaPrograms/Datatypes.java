c14c23eae825c203b24fa0ef41ff944bc2d2e547
Venkata Sriram, Gundu <Gundu.VenkataSriram@cba.com.au>
2025-01-17 21:44:34 +1100

    added

---
+++ src/JavaPrograms/Datatypes.java
@@ -0,0 +1,14 @@
+package JavaPrograms;
+
+public class Datatypes {
+    public static void main(String[] args) {
+        int age=32;
+        String name="Gv sriram";
+        float MarksPercentage=92.5f;
+        boolean result = true;
+        System.out.println(name);
+        System.out.printf("Age of sriram is %d",age);
+        System.out.printf( "Marks of sriram is %f",MarksPercentage);
+        System.out.print(result);
+    }
+}
---
+++ src/JavaPrograms/NineProgram.java
@@ -0,0 +1,17 @@
+package JavaPrograms;
+
+public class NineProgram {
+    public static void main(String[] args) {
+        int i=9;
+        System.out.printf("1*9=%d%n",i);
+        System.out.printf("2*9=%d%n",i*2);
+        System.out.printf("3*9=%d%n",i*3);
+        System.out.printf("4*9=%d%n",i*4);
+        System.out.printf("5*9=%d%n",i*5);
+        System.out.printf("6*9=%d%n",i*6);
+        System.out.printf("7*9=%d%n",i*7);
+        System.out.printf("8*9=%d%n",i*8);
+        System.out.printf("9*9=%d%n",i*9);
+        System.out.printf("10*9=%d%n",i*10);
+    }
+}
